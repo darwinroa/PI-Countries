73c9143028b9ca3ddbd2ee44526c0893739369da
Darwin Roa <darwinmichaelroamora@gmail.com>
2023-03-03 17:42:57 -0500

    Created the Tours model

---
+++ api/src/models/Tours.js
@@ -0,0 +1,34 @@
+const {
+    DataTypes
+} = require('sequelize');
+
+module.exports = (sequelize) => {
+    sequelize.define('tour', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        difficulty: {
+            type: DataTypes.STRING,
+            defaultValue: 1,
+            validate: {
+                min: 1,
+                max: 5
+            }
+        },
+        duration: {
+            type: DataTypes.TIME,
+            allowNull: false,
+        },
+        season: {
+            type: DataTypes.ENUM('summer', 'autumn', 'winter', 'spring'),
+            allowNull: false,
+        },
+    },
+    { timestamps: false } );
+}
