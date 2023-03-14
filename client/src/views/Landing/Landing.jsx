37eb37af98004f545fa644be8043158a39da9254
Darwin Roa <darwinmichaelroamora@gmail.com>
2023-03-14 11:47:26 -0500

    Ready Styles for Landing page

--- client/src/views/Landing/Landing.jsx
+++ client/src/views/Landing/Landing.jsx
@@ -1,13 +1,26 @@
 import { Link } from 'react-router-dom';
-// import style from './Landing.module.css';
+import logo from '../../img/logo-landing.webp'
+import style from './Landing.module.css';
 
 export default function Landing() {
   return (
-    <>
-      <div>Hola Mundo, vine a conquistarte</div>
-      <Link to='/home'>
-        <button>comenzar</button>
-      </Link>
-    </>
+    <div className={style.landing}>
+      <div className={style.content_img}>
+        <img src={logo} alt="Logo Countries" />
+      </div>
+      <div>
+        <h1>PI Countries</h1>
+        <h3>Develope By: Darwin Roa</h3>
+        <Link to='/home'>
+          <button class={style.cta}>
+            <span>Get started</span>
+            <svg viewBox="0 0 13 10" height="10px" width="15px">
+              <path d="M1,5 L11,5"></path>
+              <polyline points="8 1 12 5 8 9"></polyline>
+            </svg>
+          </button>
+        </Link>
+      </div>
+    </div>
   )
 }
